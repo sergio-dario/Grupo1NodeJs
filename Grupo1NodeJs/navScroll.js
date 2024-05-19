394bcc55031ab52d38b65917f8594805a9628dd3
Norman Nahuel Capcha Arcani <normannahuel@gmail.com>
2024-05-19 03:08:55 -0300

    cambios en estilo para footer(se agrega redes sociales)

--- Grupo1NodeJs/navScroll.js
+++ Grupo1NodeJs/navScroll.js
@@ -1,9 +1,5 @@
 console.log("hola soy untexto del js");
 
-// console.log(header[0]);
-//  revisar como sonseguir q imprima un cero cuando esta en el top de la pagina e imprima un uno cuando scroll
-// ee hacia abajo
-
 window.addEventListener("scroll", function () {
   let header = document.getElementsByTagName("header");
   if (window.scrollY <= 40) {
