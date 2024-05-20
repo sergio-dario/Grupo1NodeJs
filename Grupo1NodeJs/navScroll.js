2e058b06f229d5f3ee3c3566cece52b9733042d3
sergio-dario <126514929+sergio-dario@users.noreply.github.com>
2024-05-20 13:08:42 -0300

    contacto2
    contacto2

--- Grupo1NodeJs/navScroll.js
+++ Grupo1NodeJs/navScroll.js
@@ -4,6 +4,7 @@ window.addEventListener("scroll", function () {
   let header = document.getElementsByTagName("header");
   if (window.scrollY <= 40) {
     header[0].className = "initial";
+    
   } else {
     header[0].className = "scroll";
   }
