10b62c74f8d8d7a861a7594c2ee7e07a8e220245
Norman Nahuel Capcha Arcani <normannahuel@gmail.com>
2024-05-24 01:40:11 -0300

    estilos de seccion que visitas

--- js/registro.js
+++ js/registro.js
@@ -1,35 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
-    const registroExitosoDiv = document.getElementById("registroExitoso");
-    let userDataArray = JSON.parse(localStorage.getItem("userDataArray")) || [];
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const registroExitosoDiv = document.getElementById("registroExitoso");
+  let userDataArray = JSON.parse(localStorage.getItem("userDataArray")) || [];
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault(); 
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-        // Crear un objeto con los datos
-        const userData = {
-            email: email,
-            password: password
-        };
+    // Crear un objeto con los datos
+    const userData = {
+      email: email,
+      password: password,
+    };
 
-        // Agrega al array
-        userDataArray.push(userData);
+    // Agrega al array
+    userDataArray.push(userData);
 
-        // Almacena el array 
-        localStorage.setItem("userDataArray", JSON.stringify(userDataArray));
+    // Almacena el array
+    localStorage.setItem("userDataArray", JSON.stringify(userDataArray));
 
-        console.log("Datos ingresados:", userDataArray);
+    console.log("Datos ingresados:", userDataArray);
 
-        // Mostrar mensaje 
-        registroExitosoDiv.style.display = "block";
-        
+    // Mostrar mensaje
+    registroExitosoDiv.style.display = "block";
 
-        // Redirigir a la página de inicio de sesión después de unos segundos
-        setTimeout(function() {
-            window.location.href = "login.html";
-        }, 3000); 
-    });
+    // Redirigir a la página de inicio de sesión después de unos segundos
+    setTimeout(function () {
+      window.location.href = "./login.html";
+    }, 3000);
+  });
 });
