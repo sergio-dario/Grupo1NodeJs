1eb964c48a3494e4deaab5341e6c9c02ffeedb4f
sergio-dario <126514929+sergio-dario@users.noreply.github.com>
2024-06-25 15:50:19 -0300

    tabla reserva
    genera tabla cliente y reserva

--- db/db.js
+++ db/db.js
@@ -37,8 +37,8 @@ connection.connect((err) => {
                 return;
             }
 
-            // Generamos la consulta para crear la tabla
-            const createTableQuery = `
+            // Generamos la consulta para crear la tabla cliente
+            const createClienteTableQuery = `
                 CREATE TABLE IF NOT EXISTS cliente (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     nombre VARCHAR(255) NOT NULL,
@@ -47,12 +47,34 @@ connection.connect((err) => {
                 );
             `;
 
-            // Ejecutar la consulta para crear la tabla
-            connection.query(createTableQuery, (err, result) => {
+            // Ejecutar la consulta para crear la tabla cliente
+            connection.query(createClienteTableQuery, (err, result) => {
                 if (err) {
-                    console.log("Error al crear la tabla: " + err);
+                    console.log("Error al crear la tabla 'cliente': " + err);
                 } else {
                     console.log("Tabla 'cliente' creada o ya existe.");
+                    
+                    // Generamos la consulta para crear la tabla reserva
+                    const createReservaTableQuery = `
+                        CREATE TABLE IF NOT EXISTS reserva (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            cliente_id INT,
+                            provincia VARCHAR(255) NOT NULL,
+                            localidad VARCHAR(255) NOT NULL,
+                            fecha DATE NOT NULL,
+                            precio DECIMAL(10, 2) NOT NULL,
+                            FOREIGN KEY (cliente_id) REFERENCES cliente(id) ON DELETE CASCADE
+                        );
+                    `;
+
+                    // Ejecutar la consulta para crear la tabla reserva
+                    connection.query(createReservaTableQuery, (err, result) => {
+                        if (err) {
+                            console.log("Error al crear la tabla 'reserva': " + err);
+                        } else {
+                            console.log("Tabla 'reserva' creada o ya existe.");
+                        }
+                    });
                 }
             });
         });
